8a158a81d1bba8b8acda436ae9a03667fec51e18
DESKTOP-5F1MRM9\yyy <1096773067@qq.com>
2024-10-09 10:19:36 +0800

    子钱包列表BUG

--- application/admin/controller/SubWallet.php
+++ application/admin/controller/SubWallet.php
@@ -46,7 +46,7 @@ class SubWallet extends Backend
             $list = $WalletModel
                 ->with('store')
                 ->where($where)
-                ->field("id,sub_wallet_id,bind_store_id,sub_wallet_type,sub_wallet_type,discount_percentage")
+                ->field("id,sub_wallet_id,bind_store_id,sub_wallet_type,sub_wallet_type")
                 ->order($sort, $order)
                 ->limit($offset,$limit)
                 ->select();
