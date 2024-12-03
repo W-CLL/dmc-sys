0c19c5403874708dc4592b90f30ad4f9b7d3a8c1
DESKTOP-5F1MRM9\yyy <1096773067@qq.com>
2024-12-03 10:08:45 +0800

    队列列表显示

--- application/admin/controller/QueueRecord.php
+++ application/admin/controller/QueueRecord.php
@@ -28,7 +28,7 @@ Class QueueRecord extends Backend{
             $list = $this->model
                 ->where($where)
 //                ->where($where_str)
-                ->orderRaw('CASE WHEN status = 2 THEN 0 ELSE 1 END, id DESC')
+                ->order('id desc')
                 ->paginate($limit);
             foreach ($list as $item){
                 switch ($item['status']){
