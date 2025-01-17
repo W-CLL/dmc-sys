3dcf9e8a18587c27b95805650e77d4f23d249276
cll <cll@qq.com>
2025-01-17 10:02:30 +0800

    任务重启

--- application/admin/controller/QueueRecord.php
+++ application/admin/controller/QueueRecord.php
@@ -6,15 +6,17 @@ use app\common\controller\Backend;
 use app\common\model\Queue;
 use think\Db;
 
-Class QueueRecord extends Backend{
+class QueueRecord extends Backend
+{
     public $model = null;
+
     public function _initialize()
     {
         parent::_initialize();
         $this->model = model('Queue');
     }
 
-    public function  index()
+    public function index()
     {
         $this->request->filter(['strip_tags', 'trim']);
         if ($this->request->isAjax()) {
@@ -29,8 +31,8 @@ Class QueueRecord extends Backend{
                 ->where($where)
                 ->order('id desc')
                 ->paginate($limit);
-            foreach ($list as $item){
-                switch ($item['status']){
+            foreach ($list as $item) {
+                switch ($item['status']) {
                     case 0:
                         $item['status_text'] = '等待中';
                         break;
@@ -42,7 +44,7 @@ Class QueueRecord extends Backend{
                         break;
                 }
 
-                $item['msg'] = substr( $item['msg'], 0, 65);
+                $item['msg'] = substr($item['msg'], 0, 65);
             }
 
             $result = array("total" => $list->total(), "rows" => $list->items());
@@ -50,7 +52,7 @@ Class QueueRecord extends Backend{
             return json($result);
         }
         $queue = new Queue();
-        $class_list = $queue->group('class_name')->column('job_name','class_name');
+        $class_list = $queue->group('class_name')->column('job_name', 'class_name');
 
 //        $result = array_combine(
 //            array_map(function($key) {
@@ -59,28 +61,50 @@ Class QueueRecord extends Backend{
 //            $class_list
 //        );
         $result = array_combine(
-            array_map(function($key) {
+            array_map(function ($key) {
                 // 去除前缀部分
                 return str_replace('app\\job\\', '', $key);
             }, array_keys($class_list)),
             $class_list
         );
 
-        $select = build_select('class_name', $result, 0, ['class'=>'form-control selectpicker', 'data-rule'=>'required']);
+        $select = build_select('class_name', $result, 0, ['class' => 'form-control selectpicker', 'data-rule' => 'required']);
 
-        $this->assign('class_name_list',$select);
+        $this->assign('class_name_list', $select);
         return $this->view->fetch();
     }
+
     //重启任务的逻辑就是删掉原来失败的重新构建
 
-    public function rebuildOne($ids){
-        $res =  $this->model->rebuildOne($ids);
-        if($res){
+    public function rebuildOne($ids)
+    {
+        $res = $this->model->rebuildOne($ids);
+        if ($res) {
             $this->success('重启成功');
-        }else{
+        } else {
             $this->error('重启失败');
         }
     }
-    public function rebuildAll(){
+
+    //一键重启选中任务
+    public function rebuildAll($ids = '')
+    {
+        if (!$ids) {
+            $this->error('请选中一条记录');
+        }
+        $i=1;
+        $ids = explode(',',$ids);
+        foreach ($ids as $id){
+            $info = $this->model->where('job_id',$id)->find();
+            if($info['status'] == 1){
+                continue;
+            }
+            $i++;
+            $res = $this->model->rebuildOne($info['id']);
+            if(!$res){
+                $this->error('本次重启了'.$i.'条记录');
+            }
+        }
+        $this->success('重启成功');
     }
 }
--- public/assets/js/backend/queue_record.js
+++ public/assets/js/backend/queue_record.js
@@ -24,18 +24,29 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','bootstrap-table-fixed
                     [
                         {checkbox: true},
                         {field: 'id', title: __('Id'), visible: false},
-                        {field: 'job_id', title: "任务id", operate: false, searchList: Config.searchList, formatter: Table.api.formatter.label},
-                        {field: 'class_name', title:"任务类名", align: 'left', formatter:function (value, row, index) {
+                        {
+                            field: 'job_id',
+                            title: "任务id",
+                            operate: false,
+                            searchList: Config.searchList,
+                            formatter: Table.api.formatter.label
+                        },
+                        {
+                            field: 'class_name',
+                            title: "任务类名",
+                            align: 'left',
+                            formatter: function (value, row, index) {
                                 return value.toString().replace(/(&|&amp;)nbsp;/g, '&nbsp;');
                             }
                         },
                         {field: 'job_name', title: "任务名称"},
-                        {field: 'msg', title: "执行信息",width:130,align:'left'},
+                        {field: 'msg', title: "执行信息", width: 130, align: 'left'},
                         {field: 'remark', title: "备注"},
                         {field: 'status_text', title: __('Status')},
-                        {field: 'create_time', title:"创建时间" ,formatter: Table.api.formatter.datetime},
-                        {field: 'operate', title: __('Operate'),
-                            buttons:[{
+                        {field: 'create_time', title: "创建时间", formatter: Table.api.formatter.datetime},
+                        {
+                            field: 'operate', title: __('Operate'),
+                            buttons: [{
                                 name: "queue_records",
                                 text: "重启",//按钮名称
                                 classname: 'btn btn-xs btn-success  btn-ajax',
@@ -47,9 +58,9 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','bootstrap-table-fixed
                                 refresh: true,
                                 visible: function (row) {
                                     //返回true时按钮显示,返回false隐藏
-                                    if(row.status == 2){
+                                    if (row.status == 2) {
                                         return true;
-                                    }else{
+                                    } else {
                                         return false;
                                     }
                                 },
@@ -57,7 +68,8 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','bootstrap-table-fixed
                                 //     table.bootstrapTable('refresh');
                                 // }
                             }],
-                            table: table, events: Table.api.events.operate, formatter: Table.api.formatter.buttons}
+                            table: table, events: Table.api.events.operate, formatter: Table.api.formatter.buttons
+                        }
                     ]
                 ]
             };
@@ -65,6 +77,64 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','bootstrap-table-fixed
             table.bootstrapTable(tableOptions);
             // 为表格绑定事件
             Table.api.bindevent(table);
+            table.on('check-all.bs.table', function (e, rows) {
+                // 点击全选触发事件
+                var select_total = 0;
+                for (i = 0; i < rows.length; i++) {
+                    select_total = select_total + 1;
+                }
+                $("#select_total").text(select_total);
+            })
+
+            table.on('uncheck-all.bs.table', function (e, rows) {
+                // 点击反选触发事件
+                $("#select_total").text("0");
+            })
+
+            table.on('check.bs.table', function (e, row) {
+                // 勾选某一行触发事件
+                var select_total = parseInt($("#select_total").text()) + 1;
+                $("#select_total").text(select_total);
+            })
+
+            table.on('uncheck.bs.table', function (e, row) {
+                // 反选某一行触发事件
+                var select_total = parseInt($("#select_total").text()) - 1;
+                $("#select_total").text(select_total);
+            })
+
+            table.on('post-body.bs.table', function (e, row) {
+                $("#select_total").text("0");
+            })
+
+
+            /* 获取选中的id */
+            function getIdSelections() {
+                return $.map($("#table").bootstrapTable('getSelections'), function (row) {
+                    return row.job_id
+                });
+            }
+
+            $(".btn-edits").on('click', function () {
+                var checkids = [];
+                checkids = getIdSelections();
+                // 使用JavaScript通过类名选择器找到隐藏的输入字段
+                layer.confirm('是否确认一键重启?(只能重启等待中/失败的任务)', {
+                        btn: ['确认', '取消']
+                    },
+                    function (index) {
+                        Fast.api.ajax({
+                            url: 'queue_record/rebuildAll',
+                            data: {
+                                ids: checkids.join(','),
+                            }
+                        }, function (data, ret) {
+                            layer.msg(ret.msg);
+                            table.bootstrapTable('refresh', {});
+                            Layer.close(index);
+                        });
+                    })
+            })
         },
         api: {
             bindevent: function () {
