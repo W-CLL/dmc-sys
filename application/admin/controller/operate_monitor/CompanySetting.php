b63b1ba6d13d324346a9d08d20d20d4e607b1e8a
cll <cll@qq.com>
2025-02-07 15:38:50 +0800

    fixed search

--- application/admin/controller/operate_monitor/CompanySetting.php
+++ application/admin/controller/operate_monitor/CompanySetting.php
@@ -40,9 +40,16 @@ class CompanySetting extends Backend
         if ($this->request->isAjax()) {
             $offset = input("offset", 0);
             $limit = input("limit", 10);
+            $input = json_decode(input('filter'),true);
+
             $list = $this->companyModel
                 ->alias('c')
                 ->join('company_setting cs', 'c.company_name = cs.company_name', 'left')
+                ->where(function ($query) use ($input){
+                    if(isset($input['company_name'])){
+                        $query->where(['c.company_name'=>['like',"%".$input['company_name']."%"]]);
+                    }
+                })
                 ->field("c.company_name,cs.is_white,cs.percentage,cs.id,count(c.id) as adv_num")
                 ->group('c.company_name')
                 ->order('adv_num desc')
@@ -53,7 +60,13 @@ class CompanySetting extends Backend
                 $item['percentage'] = $item['percentage']."%";
             }
 
-            $count = $this->companyModel->group('company_name')->count();
+            $count = $this->companyModel
+                ->where(function ($query) use ($input){
+                    if(isset($input['company_name'])){
+                        $query->where(['company_name'=>['like',"%".$input['company_name']."%"]]);
+                    }
+                })
+                ->group('company_name')->count();
             $result = array("total" => $count, "rows" => $list);
             return json($result);
         }
