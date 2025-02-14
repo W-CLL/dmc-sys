3488077500f333c1af2403af6d92f5832a302e46
cll <cll@qq.com>
2025-02-15 00:31:25 +0800

    拉取全域数据

--- application/qcdatahandle/controller/InitQcAdvCost.php
+++ application/qcdatahandle/controller/InitQcAdvCost.php
@@ -183,10 +183,15 @@ class InitQcAdvCost extends Controller
     public function initGetGlobalCostVideo($marketing_goal = "VIDEO_PROM_GOODS")
     {
         $redis = Cache::store('redis');
+        if( !$redis->get('global_cost_date_' . $marketing_goal)){
+            echo "2月1到2月14的数据已经全部获取完了";
+            die;
+        }
         $date = $redis->get('global_cost_date_' . $marketing_goal, '2025-02-01');
         $page = Cache::get('global_cost_page_' . $marketing_goal . '_' . $date, 1);
         $model = new Company();
         $adv_list = $model->page($page)->limit(20)->order('id desc')->column('advertiser_id');
+
         if ($date == '2025-02-15' || $date == '2025-2-15') {
             echo "2月1到2月14的数据已经全部获取完了";
             die;
