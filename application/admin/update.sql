cf60824a83c75c77b857e62627990a4c2fb2ca8b
DESKTOP-5F1MRM9\yyy <204623001@qq.com>
2025-06-13 15:07:20 +0800

    1

--- application/admin/update.sql
+++ application/admin/update.sql
@@ -301,10 +301,10 @@ CREATE TABLE `fa_mark_log` (
                                `admin_id` int(11) NOT NULL,
                                `operator` varchar(100) NOT NULL COMMENT '操作人',
                                `adv_id` varchar(50) NOT NULL COMMENT '千川id',
-                               `title` varchar(100) NOT NULL COMMENT '标题',
                                `content` longtext NOT NULL COMMENT '内容',
                                `create_time` bigint(11) NOT NULL,
-                               PRIMARY KEY (`id`)
+                               PRIMARY KEY (`id`),
+                               KEY `normal` (`admin_id`,`operator`,`adv_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
