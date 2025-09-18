edc501d5e1a4b312d37637e3b6459cc58d9fecd1
DESKTOP-5F1MRM9\yyy <204623001@qq.com>
2025-09-18 17:49:29 +0800

    拉取子钱包+优化

---
+++ application/admin/model/TencentShareWallet.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\admin\model;
+
+use think\Model;
+
+class TencentShareWallet extends Model
+{
+    // 表名
+    protected $name = 'tencent_share_wallet';
+    
+    // 主键
+    protected $pk = 'id';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = false;
+    
+    // 不隐藏任何字段
+    protected $hidden = [];
+    
+    // 明确指定可见字段，确保包含id和store关联字段
+    protected $visible = ['id', 'store_id', 'sub_wallet_id', 'sub_wallet_name', 'wallet_type', 'discount_percentage', 'store'];
+    
+    // 关联店铺模型
+    public function store()
+    {
+        return $this->belongsTo('app\admin\model\Store', 'store_id', 'id');
+    }
+}
