56fd882050c9a5a08c92d3df953ab1c41173ef9d
cll <cll@qq.com>
2025-09-26 14:02:16 +0800

    企业微信通知_补单用

---
+++ application/api/controller/WwNotice.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+use think\Cache;
+use think\Controller;
+
+/**
+ * 示例接口
+ */
+class WwNotice extends Controller
+{
+    public function sendAutoOrderMsg($auth, $msg = '测试不用管')
+    {
+//        dump($auth);
+        if ($auth != "auto-order") {
+            $this->error('非法请求');
+        }
+        \qywx\Api::send_application_messages('WangChunLong', $msg);
+
+
+    }
+
+}
