2e1d5e77bb668e8a60194e0bcc594d6eeb9cf8e9
DESKTOP-5F1MRM9\yyy <204623001@qq.com>
2025-11-03 13:45:12 +0800

    腾讯认款充值

---
+++ application/store/controller/tencent/Recharge.php
@@ -0,0 +1,276 @@
+<?php
+
+namespace app\store\controller\tencent;
+
+
+use app\common\controller\Store;
+
+use qywx\Api;
+use think\Cache;
+use think\Db;
+use txy\TextRecognition;
+
+
+class Recharge extends Store
+{
+
+    protected static $payee = [
+        "广州浣熊数字信息科技有限公司",
+        "罗文静",
+        "*文静",
+        "罗*静",
+        "吴忠杰",
+        "*忠杰",
+        "吴*杰",
+        "黄娜",
+        "*娜",
+    ];
+    protected static $unique_identifier = [
+        "日志号",
+        "交易序号",
+        "凭证号",
+        "交易流水号",
+        "电子回单号",
+        "网银交易流水号",
+        "指令序号",
+        "汇款编号",
+        "回单号",
+        "受理单号",
+        "回单流水号",
+        "柜员交易号",
+        "转账流水号",
+        "流水号",
+        "凭证号",
+        "回单验证码"
+    ];
+
+    protected static $money_name = [
+        "金额",
+        "金额小写",
+        "汇款金额",
+        "小写金额",
+        "转账金额",
+        "金额(小写)",
+        "收款金额",
+        "转账汇款金额",
+        "币种及金额(大小写)",
+        "金额/币种",
+        "交易金额",
+    ];
+
+    protected static $payee_key = [
+        "收款人",
+        "收款人名称",
+        "收款人全称",
+        "收款单位",
+        "收款方户名",
+        "收款户名",
+        "收款人姓名",
+        "收款人户名",
+        "户名",
+        "姓名", // ocr识别失败?返回一个"姓名"键
+    ];
+   // 修改这些字段需要同时修改 Recharge.php
+
+    public function index()
+    {
+
+        if ($this->request->isAjax()) {
+//            $type = input("type");
+            $store = Db::name("tencent_store")->where(['store_id' => $this->auth->id])->find();
+            $store_info = Db::name("store")->where("id", $store['store_id'])->find();
+
+//            if ($type == 1){
+
+            $order_num = input("order_num");
+            $redis = Cache::store('redis')->handler();
+            $order = $redis->get($order_num);
+            if (empty($order)) {
+                $this->error("充值失败，请刷新后重试");
+            }
+            $redis->del($order_num);
+            $order = json_decode($order, true);
+            if (Db::name("tencent_transaction_log")->where("order_number", $order['order_number'])->count()) {
+                $this->error("该回单已充值");
+            }
+            if (Db::name('receipt_use_log')->where("receipt_no", $order['order_number'])->count()){
+                $this->error("该回单已使用");
+            }
+            Db::startTrans();
+            try {
+                if ($order['account_type'] == 1) {
+                    $before_money = $store['public_money_tencent'];
+                    $before_limit = $store['public_credit_limit_tencent'];
+                    $actual_money = $order['money'];
+                    $deduction_credit_limit = 0;
+                    $explain = "充值公账钱包" . $order['money'] . "元";
+                    if ($store['public_spending_credit_limit_tencent'] > 0) {
+                        $explain .= ",已使用公账授信额度" . $store['public_spending_credit_limit_tencent'] . "元,";
+                        if ($store['public_spending_credit_limit_tencent'] >= $order['money']) {
+                            if (!Db::name("tencent_store")->where(['id' => ["=", $store['id']], 'public_spending_credit_limit_tencent' => ['>=', $order['money']]])->setDec('public_spending_credit_limit_tencent', $order['money'])) {
+                                throw new \Exception('扣除授信额度失败');
+                            }
+                            $actual_money = 0;
+                            $deduction_credit_limit = $order["money"];
+                            $explain .= "扣除" . $order['money'] . "元";
+                        } else {
+                            $actual_money = $order['money'] - $store['public_spending_credit_limit_tencent'];
+                            $deduction_credit_limit = $store['public_spending_credit_limit_tencent'];
+
+                            Db::name("tencent_store")->where('id', $store['id'])->update(['public_spending_credit_limit_tencent' => 0]);
+                            $explain .= "扣除" . $store['public_spending_credit_limit_tencent'] . "元";
+                        }
+                        $explain .= "实际到账" . $actual_money . "元";
+                        Db::name("tencent_store")->where("id", $store['id'])->setInc("public_credit_limit_tencent", $deduction_credit_limit);
+                    }
+                    if ($actual_money > 0) {
+                        Db::name("tencent_store")->where(['id' => $store['id']])->setInc("public_money_tencent", $actual_money);
+                    }
+                } else {
+                    $before_money = $store['private_money_tencent'];
+                    $before_limit = $store['private_credit_limit_tencent'];
+                    $actual_money = $order['money'];
+                    $deduction_credit_limit = 0;
+                    $explain = "充值私账钱包" . $order['money'] . "元";
+                    if ($store['private_spending_credit_limit_tencent'] > 0) {
+                        $explain .= ",已使用私账授信额度" . $store['private_spending_credit_limit_tencent'] . "元,";
+                        if ($store['private_spending_credit_limit_tencent'] >= $order['money']) {
+                            if (!Db::name("tencent_store")->where(['id' => ["=", $store['id']], 'private_spending_credit_limit_tencent' => ['>=', $order['money']]])->setDec('private_spending_credit_limit_tencent', $order['money'])) {
+                                throw new \Exception('扣除授信额度失败');
+                            }
+                            $actual_money = 0;
+                            $deduction_credit_limit = $order["money"];
+                            $explain .= "扣除" . $order['money'] . "元";
+                        } else {
+                            $actual_money = $order['money'] - $store['private_spending_credit_limit_tencent'];
+                            $deduction_credit_limit = $store['private_spending_credit_limit_tencent'];
+                            Db::name("tencent_store")->where('id', $store['id'])->update(['private_spending_credit_limit_tencent' => 0]);
+                            $explain .= "扣除" . $store['private_spending_credit_limit_tencent'] . "元";
+                        }
+                        $explain .= "实际到账" . $actual_money . "元";
+                        Db::name("tencent_store")->where("id", $store['id'])->setInc("private_credit_limit_tencent", $deduction_credit_limit);
+                    }
+                    if ($actual_money > 0) {
+                        Db::name("tencent_store")->where(['id' => $store['id']])->setInc("private_money_tencent", $actual_money);
+                    }
+                }
+
+                Db::name("tencent_transaction_log")->insert([
+                    "store_id" => $store_info["id"],
+                    "username" => $store_info["username"],
+                    "money" => $order['money'],
+                    "actual_money" => $actual_money,
+                    "account_type" => $order["account_type"],
+                    "deduction_credit_limit" => $deduction_credit_limit,
+                    "receipt_image" => $order['image'],
+                    "before_money" => $before_money,
+                    "today_money" => $before_money + $actual_money,
+                    "order_number" => $order["order_number"],
+                    "type" => 3,
+                    "explain" => $explain,
+                    "create_time" => time(),
+                    "balance_surplus" => $before_money + $actual_money,
+                    "credit_limit_surplus" => $before_limit + $deduction_credit_limit
+                ]);
+                // 提交事务
+                Db::commit();
+            } catch (\Exception $e) {
+                // 回滚事务
+                Db::rollback();
+                $this->error($e->getMessage());
+            }
+
+            $user_ids = Db::name("financial_staff")->where(["state" => 1])->column("user_id");
+            if (!empty($user_ids)) {
+                $media_id = Api::media_upload(ROOT_PATH . "public" . $order['image']);
+                if (!empty($media_id)) {
+                    $user_ids = implode("|", $user_ids);
+                    Api::send_image_messages($user_ids, $media_id);
+                }
+            }
+            $this->success();
+        }
+        $data = Db::name("tencent_store")
+            ->where('store_id', $this->auth->id)
+            ->field('public_money_tencent,private_money_tencent,public_credit_limit_tencent,private_credit_limit_tencent,
+            public_spending_credit_limit_tencent,private_spending_credit_limit_tencent,public_discount_percentage_tencent,private_discount_percentage_tencent')->find();
+        $this->assign("data", $data);
+        return $this->view->fetch();
+    }
+
+    public function get_image_info()
+    {
+
+        $image = input("image");
+        $config_data = Db::name("qc_config")->where("id", 2)->find();
+        $data = TextRecognition::get_image_info($config_data['secret'], $config_data['api_key'], request()->domain() . $image);
+        $money = 0;
+        $payee = '';
+        $order_number = '';
+        $account_type = 0;
+
+        foreach ($data['BankSlipInfos'] as $k => $v) {
+            if (!$money) {
+                foreach (self::$money_name as $key => $vel) {
+                    if ($v['Name'] == $vel) {
+                        $money = floatval(preg_replace('/[^\d.]/', '', $v['Value']));
+                        break;
+                    }
+                }
+            }
+            if (!$payee) {
+                foreach (self::$payee_key as $payee_name) {
+                    if ($v["Name"] == $payee_name) {
+                        $payee = $v['Value'];
+
+                        foreach (self::$payee as $key => $vel) {
+                            if ($v["Value"] == $vel) {
+                                if ($key == 0) {
+                                    $account_type = 1;
+                                } else {
+                                    $account_type = 2;
+                                }
+                                break;
+                            }
+                        }
+                        break;
+                    }
+
+                }
+            }
+            if ($account_type == 0) {
+                $payee = '';
+            }
+            if (!$order_number) {
+                foreach (self::$unique_identifier as $key => $vel) {
+                    if ($v["Name"] == $vel) {
+                        if (ctype_digit($v['Value']) && $v["Value"] != 0) {
+                            if (!empty((int)$v['Value'])) {
+                                $order_number = $v['Value'];
+                            }
+                        } else {
+                            if (!empty($v['Value'])) {
+                                $order_number = $v['Value'];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if ($account_type == 0) {
+            return json(['code' => 0, "msg" => "识别失败,请检查回单"]);
+        }
+
+        if ($money && $payee && $order_number) {
+            if (Db::name("tencent_transaction_log")->where("order_number", $order_number)->count()) {
+                return json(['code' => 0, "msg" => "该回单已充值"]);
+            }
+            $order_num = date('YmdHis') . mt_rand(1000, 9999);
+            Cache::store("redis")->set($order_num, json_encode(['money' => $money, 'account_type' => $account_type, "order_number" => $order_number, 'image' => $image], JSON_UNESCAPED_UNICODE), 3600);
+            return json(['code' => 1, "msg" => "请求成功", "data" => ['money' => $money, 'payee' => $payee, 'account_type' => $account_type, 'order_num' => $order_num]]);
+        }
+        return json(['code' => 0, "msg" => "识别失败"]);
+    }
+
+}
+
---
+++ public/assets/js/store/tencent/recharge.js
@@ -0,0 +1,77 @@
+define(['jquery', 'bootstrap', 'store', 'table', 'form'], function ($, undefined, Store, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            Controller.api.bindevent();
+            $("#faupload-local").data("upload-success", function(data, ret){
+                $.ajax({
+                    url: 'tencent/recharge/get_image_info',
+                    dataType: 'json',
+                    data: {image: data.url},
+                    cache: false,
+                    success: function (ret) {
+                        if (ret.code){
+                            $("#money").text(ret.data.money);
+                            $("#payee").text(ret.data.payee);
+                            $("#order_num").val(ret.data.order_num)
+                            if (ret.data.account_type == 1){
+                                $("#account_type").text('公对公')
+                            }else{
+                                $("#account_type").text('私对私')
+                            }
+                            $("#image_info").css("display","block")
+                        }else{
+                            Toastr.error(ret.msg);
+                        }
+                    }, error: function () {
+                        Toastr.error(__('Network error'));
+                    }
+                });
+            });
+            // 给表单绑定事件
+            Form.api.bindevent($("#edit-form"), function () {
+
+                setTimeout(function () {
+                    location.reload();
+                }, 1500);
+                return true;
+            }, function() {
+                // 提交完成后重新启用按钮
+                $(".btn-primary").removeClass("disabled");
+            }, function() {
+                // 提交前禁用按钮
+                $(".btn-primary").addClass("disabled");
+            });
+
+            $('#type').on('change', function() {
+                if (this.value == 1){
+                    $('#type1').css('display','block')
+                    $('#type2').css('display','none')
+                }else{
+                    $('#type1').css('display','none')
+                    $('#type2').css('display','block')
+                }
+            })
+
+            $("#example").on('click',function(){
+
+                layer.open({
+                    type: 1,
+                    area: ['680px', '520px'],
+                    content: "<h5 style='margin: 20px'>" +
+                        "<p>需明确可见甲方付款人抬头、日期、金额以及乙方收款抬头、银行卡号、银行信息。</p>" +
+                        "<p>图片打码是隐藏客户信息，上传请勿打码！以免系统识别不到！</p>" +
+                        "<img style='width: 600px' src='/example/1718868742692.jpg'>"+
+                        "<img style='width: 600px' src='/example/1718868827655.jpg'>"+
+                        "</h5>" //这里content是一个普通的String
+                });
+            })
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
