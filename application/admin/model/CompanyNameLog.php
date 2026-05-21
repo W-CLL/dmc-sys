9761e9a7329e42aec659330dd0bcc865f3200260
cll <cll@qq.com>
2026-05-21 17:42:59 +0800

    公司主体变更通知

---
+++ application/admin/model/CompanyNameLog.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\admin\model;
+
+
+use app\admin\controller\QueueRecord;
+use app\common\model\Queue;
+use think\Model;
+
+class CompanyNameLog extends Model
+{
+    // 表名
+    protected $name = 'company_name_log';
+
+}
